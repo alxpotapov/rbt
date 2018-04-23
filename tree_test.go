2802ec3ad225e3ef01a03a8bc965e08fc81fc25e
apotapov <al.ptp@icloud.com>
2018-04-23 20:01:12 +0300

    Red-black tree

---
+++ tree_test.go
@@ -0,0 +1,27 @@
+package rbt
+
+import (
+	"testing"
+)
+
+
+func TestNode_Insert(t *testing.T) {
+	tree := &Tree{}
+	tree.Insert("00", 0)
+	tree.Insert("01", 1)
+	tree.Insert("02", 2)
+	tree.Insert("03", 3)
+	tree.Insert("04", 4)
+	tree.Insert("05", 5)
+	tree.Insert("06", 6)
+	tree.Insert("07", 7)
+	tree.Insert("08", 8)
+	tree.Insert("09", 9)
+	tree.Insert("00", 0)
+	tree.Insert("10", 10)
+	tree.Insert("11", 11)
+	if err := tree.checkViolation(); err != nil {
+		t.Fatal(err)
+	}
+	t.Log(tree.String())
+}
